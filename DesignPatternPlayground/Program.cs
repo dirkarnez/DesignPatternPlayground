6fa464a31f48ba5f4aa55acd8527e29c287efc58
dirkarnez <smalldirkalex@gmail.com>
2018-12-06 11:20:15 +0800

    - init and add StrategyPattern

---
+++ DesignPatternPlayground/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace DesignPatternPlayground
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            new StrategyPatternProvider().GetById("B").SayHi();
+            Console.ReadLine();
+        }
+    }
+}
---
+++ DesignPatternPlayground/StrategyPattern.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace DesignPatternPlayground
+{
+    public class StrategyPatternProvider
+    {
+        private ICollection<StrategyPattern> _strategyPatterns;
+
+        public StrategyPattern GetById(string Id)
+        {
+            LoadData();
+            return _strategyPatterns.SingleOrDefault(sp => sp.Id == Id);
+        }
+
+        private void LoadData()
+        {
+            if (_strategyPatterns != null)
+            {
+                return;
+            }
+
+            _strategyPatterns = new List<StrategyPattern>()
+            {
+                new StrategyPatternA(),
+                new StrategyPatternB()
+            };
+        }
+    }
+
+    public abstract class StrategyPattern : IEquatable<StrategyPattern>
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+
+        public abstract void SayHi();
+
+        public bool Equals(StrategyPattern other)
+        {
+            return other != null && Id == other.Id;
+        }
+    }
+
+    public class StrategyPatternA : StrategyPattern
+    {
+        public StrategyPatternA()
+        {
+            Id = "A";
+            Name = "StrategyPatternA";
+        }
+
+        public override void SayHi()
+        {
+            Console.WriteLine($"{Name} is speaking");
+        }
+    }
+
+    public class StrategyPatternB : StrategyPattern
+    {
+        public StrategyPatternB()
+        {
+            Id = "B";
+            Name = "StrategyPatternB";
+        }
+
+        public override void SayHi()
+        {
+            Console.WriteLine($"{Name} is walking");
+        }
+    }
+}
